2452b5413fea8e6ee53d789591cf6f9338963045
荒木真 <lineinfo@infoseek.jp>
2020-05-16 11:49:48 +0900

    モデル作成まで

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root "messages#index"
   resources  :users, only: %i(edit update)
+  resources  :groups, only: %i(new create edit update)
 end
