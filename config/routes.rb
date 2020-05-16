640873aa1fc9e21521e8170db1a69d2647cb4e90
荒木真 <lineinfo@infoseek.jp>
2020-05-16 12:15:04 +0900

    routes変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root "messages#index"
+  root 'groups#index'
   resources  :users, only: %i(edit update)
   resources  :groups, only: %i(new create edit update)
 end
