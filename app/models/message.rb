0f28a64f7480eab6eebd56d1b6620ff426e3d5ef
荒木真 <lineinfo@infoseek.jp>
2020-05-16 17:02:15 +0900

    ビューの変更

--- app/models/message.rb
+++ app/models/message.rb
@@ -5,4 +5,8 @@ class Message < ApplicationRecord
   validates :content, presence: true, unless: :image?
 
   mount_uploader :image, ImageUploader
+
+  def date
+    created_at.strftime("%Y年%m月%d日 %H時%M分")
+  end
 end
