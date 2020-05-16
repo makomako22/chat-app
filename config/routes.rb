f73fed82240f983391b50fa512e78dd1aedd52fd
荒木真 <lineinfo@infoseek.jp>
2020-05-16 12:36:44 +0900

    ルーティング編集

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'groups#index'
   resources  :users, only: %i(edit update)
-  resources  :groups, only: %i(index new create edit update)
+  resources  :groups, only: %i(index new create edit update) do
+    resources  :messages, only: %i(index create)
+  end
 end
