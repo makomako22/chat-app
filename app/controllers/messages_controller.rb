b9ba23283c2dd3cafeb8350261fe1bc7b0ec152c
荒木真 <lineinfo@infoseek.jp>
2020-05-16 12:44:09 +0900

    messagesコントローラー編集

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,15 @@
 class MessagesController < ApplicationController
   def index
+    @group = Group.find(params[:id])
+    @messeages = @group.messages
   end
+
+def create
+  @message = message.new(message_params)
+  if @message.save
+    redirect_to group_messages_path(@message.group)
+  else
+    render :index
+end
+
 end
