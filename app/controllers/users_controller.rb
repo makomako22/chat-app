a36ffbeae31ea18ca0cf046dbe6e244c6aba1ab5
荒木真 <lineinfo@infoseek.jp>
2020-05-17 10:33:20 +0900

    usersコントローラーindexアクション定義

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,20 @@
 class UsersController < ApplicationController
 
+  def index
+    respond_to do |format|
+      format.json
+    end
+  end
+
   def edit; end
 
-def update
-  if current_user.update(user_params)
-    redirect_to root_path
-  else
-    render :edit
+  def update
+    if current_user.update(user_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
   end
-end
 
   private
 
