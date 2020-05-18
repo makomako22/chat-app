4a9303925cd6cce18286355311d3a1ed52bb4707
荒木真 <lineinfo@infoseek.jp>
2020-05-18 11:36:58 +0900

    edit production

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,6 @@
 server '52.197.55.61', user: 'ec2-user', roles: %w{app db web},
   ssh_options: {
-    port: 22022,
+    port: 22,
     user: "ec2@user",
     keys: %w(~/.ssh/chatapp.pem),
     forward_agent: true
