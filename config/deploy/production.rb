575acef8a57ac816ef108d3bb70baa7622dc43fc
荒木真 <lineinfo@infoseek.jp>
2020-05-19 17:01:48 +0900

    add config

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,7 +1,1 @@
-server '52.197.55.61', user: 'ec2-user', roles: %w{app db web},
-  ssh_options: {
-    port: 22,
-    user: "ec2@user",
-    keys: %w(~/.ssh/chatapp.pem),
-    forward_agent: true
-  }
+server '52.197.55.61', user: 'ec2-user', roles: %w{app db web}
